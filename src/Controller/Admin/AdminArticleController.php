1e6082b6b351cc04565c0109172d6c693bbf998d
RaphaelPortailler <raphael.portailler@lapiscine.pro>
2024-08-02 12:27:48 +0200

    ajout bouton suppression pour admin

--- src/Controller/Admin/AdminArticleController.php
+++ src/Controller/Admin/AdminArticleController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller\Admin;
 
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,6 +21,27 @@ class AdminArticleController extends AbstractController
         return $this->render('admin/articles.html.twig', [
             'articles' => $articles
         ]);
+    }
+
+
+    #[Route('/admin/articles/delete/{id}', name: 'delete_articles')]
+    public function deleteArticle(int $id, ArticleRepository $articleRepository, EntityManagerInterface $entityManager): Response
+    {
+        $articles = $articleRepository->find($id);
+
+        if (!$articles) {
+            $html = $this->renderView('admin/page/404.html.twig');
+            return new Response($html, 404);
+        }
+
+        // j'utilise la classe entity manager
+        // pour préparer la requête SQL de suppression
+        // cette requête n'est pas executée tout de suite
+        $entityManager->remove($articles);
+        // j'execute la / les requête SQL préparée
+        $entityManager->flush();
+
+        return $this->redirectToRoute('admin_articles');
     }
 
 
